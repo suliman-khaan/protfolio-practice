5fb5cd9f95060eb8d420fbb6e5f1b01efcf1a375
sulimank418 <sulimank418@gmail.com>
2021-12-14 21:17:39 +0500

    Create the new secton

--- js/main.js
+++ js/main.js
@@ -7,7 +7,14 @@ window.onload = function fun_dig(){
     toggle.addEventListener('click',function(event){
         collapse.classList.toggle("active");
         // console.log("aaa");
-    });
+    }); 
+    let grid = document.querySelector("#main-site .recent-work-area .images-flex");
+    let mosonary = new Masonry(grid,{
+        itemSelector:".flex-item",
+        gutter: 50,
+        fitWidth:true
+    })
+
 }
 
 // console.log(document.querySelector("body"));
